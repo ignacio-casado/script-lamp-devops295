4cc458e7ac9a69bb14ff78348c274d3ef2d27ccc
Ignacio casado <nacho.casado01@gmail.com>
2023-11-13 21:07:36 -0300

    terminado

--- script1.sh
+++ script1.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
 # Verificar si el usuario es root
-if [ "$EUID" -ne 0 ]
-then
+if [ "$EUID" -ne 0 ]; then
     echo "Este script debe ser ejecutado como root. Usa 'sudo' para ejecutarlo."
-    exit
+    exit 1
 fi
 
-# Verificar si Apache ya está instalado
-if ! command -v apache2 &> /dev/null
-then
-    # Instalar Apache
+# Verificar e instalar Apache
+if ! command -v apache2 &> /dev/null; then
+    echo "Instalando Apache..."
     sudo apt update
     sudo apt install -y apache2
 else
     echo "Apache ya está instalado."
 fi
 
-# Verificar si MariaDB ya está instalado
-if ! command -v mariadb &> /dev/null
-then
-    # Instalar MariaDB
+# Verificar e instalar MariaDB
+if ! command -v mariadb &> /dev/null; then
+    echo "Instalando MariaDB..."
     sudo apt install -y mariadb-server
 else
     echo "MariaDB ya está instalado."
 fi
 
-# Verificar si PHP ya está instalado
-if ! command -v php &> /dev/null
-then
-    # Instalar PHP y extensiones
+# Verificar e instalar PHP
+if ! command -v php &> /dev/null; then
+    echo "Instalando PHP y extensiones..."
     sudo apt install -y php libapache2-mod-php php-mysql
 else
     echo "PHP ya está instalado."
@@ -39,16 +35,17 @@ fi
 sudo systemctl restart apache2
 sudo systemctl restart mariadb
 
-# Clonar el repositorio
+# Clonar el repositorio (reemplaza <URL_DEL_REPOSITORIO>)
 git clone <URL_DEL_REPOSITORIO>
 
-# Copiar los archivos al directorio /var/www/html
+# Copiar archivos al directorio /var/www/html
 sudo cp -r nombre_del_repo/* /var/www/html/
 
 # Realizar un respaldo del archivo index.html
 sudo mv /var/www/html/index.html /var/www/html/index.html.bkp
 
 # Testear el servidor web utilizando curl
+echo "Probando el servidor web..."
 curl http://localhost
 
 # Ruta del archivo index.php
@@ -64,7 +61,10 @@ fi
 cp "$archivo_index" "${archivo_index}.backup"
 
 # Modificar la dirección IP a localhost usando awk
-awk '{gsub(/172\.20\.1\.101/, "localhost")}1' "$archivo_index" > "${archivo_index}.temp"
-mv "${archivo_index}.temp" "$archivo_index"
-
-echo "La dirección IP en el archivo $archivo_index ha sido modificada a localhost con éxito."
+if command -v awk &> /dev/null; then
+    awk '{gsub(/172\.20\.1\.101/, "localhost")}1' "$archivo_index" > "${archivo_index}.temp"
+    mv "${archivo_index}.temp" "$archivo_index"
+    echo "La dirección IP en el archivo $archivo_index ha sido modificada a localhost con éxito."
+else
+    echo "ATENCIÓN: 'awk' no está instalado. La modificación del archivo index.php no se realizó."
+fi
